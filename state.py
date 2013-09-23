916b996ff152f215d4adaccef7a368c0aea87008
Jeroen <osscca.git@gmail.com>
2013-09-23 14:51:32 +0200

    Few minor changes.

--- state.py
+++ state.py
@@ -1,63 +1,63 @@
-"""Implementation of the state pattern"""  
+"""Implementation of the state pattern"""
 
-'''http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/'''
+# http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
 
 
 class State(object):
-    """Base state. This is to share functionality"""      
+    """Base state. This is to share functionality"""
 
-    def scan(self):         
-        """Scan the dial to the next station"""         
-        self.pos += 1         
-        if self.pos == len(self.stations):             
-            self.pos = 0         
-        print("Scanning... Station is", self.stations[self.pos], self.name)  
+    def scan(self):
+        """Scan the dial to the next station"""
+        self.pos += 1
+        if self.pos == len(self.stations):
+            self.pos = 0
+        print("Scanning... Station is", self.stations[self.pos], self.name)
 
 
 class AmState(State):
-    def __init__(self, radio):         
-        self.radio = radio         
-        self.stations = ["1250", "1380", "1510"]         
-        self.pos = 0         
-        self.name = "AM"      
+    def __init__(self, radio):
+        self.radio = radio
+        self.stations = ["1250", "1380", "1510"]
+        self.pos = 0
+        self.name = "AM"
 
-    def toggle_amfm(self):         
-        print("Switching to FM")         
-        self.radio.state = self.radio.fmstate  
+    def toggle_amfm(self):
+        print("Switching to FM")
+        self.radio.state = self.radio.fmstate
 
 
 class FmState(State):
-    def __init__(self, radio):         
-        self.radio = radio         
-        self.stations = ["81.3", "89.1", "103.9"]         
-        self.pos = 0         
-        self.name = "FM"      
+    def __init__(self, radio):
+        self.radio = radio
+        self.stations = ["81.3", "89.1", "103.9"]
+        self.pos = 0
+        self.name = "FM"
 
-    def toggle_amfm(self):         
-        print("Switching to AM")         
-        self.radio.state = self.radio.amstate  
+    def toggle_amfm(self):
+        print("Switching to AM")
+        self.radio.state = self.radio.amstate
 
 
 class Radio(object):
-    """A radio.     It has a scan button, and an AM/FM toggle switch."""      
-    def __init__(self):         
-        """We have an AM state and an FM state"""          
-        self.amstate = AmState(self)         
-        self.fmstate = FmState(self)         
-        self.state = self.amstate      
+    """A radio.     It has a scan button, and an AM/FM toggle switch."""
+    def __init__(self):
+        """We have an AM state and an FM state"""
+        self.amstate = AmState(self)
+        self.fmstate = FmState(self)
+        self.state = self.amstate
 
-    def toggle_amfm(self):         
-        self.state.toggle_amfm()     
+    def toggle_amfm(self):
+        self.state.toggle_amfm()
 
-    def scan(self):         
-        self.state.scan()  
+    def scan(self):
+        self.state.scan()
 
 
 # Test our radio out
 if __name__ == '__main__':
-    radio = Radio() 
-    actions = [radio.scan] * 2 + [radio.toggle_amfm] + [radio.scan] * 2 
-    actions = actions * 2 
+    radio = Radio()
+    actions = [radio.scan] * 2 + [radio.toggle_amfm] + [radio.scan] * 2
+    actions *= 2
 
-    for action in actions:     
+    for action in actions:
         action()
