450bf3e2fb663f299455a30c947368a0215ea21f
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-01-02 03:59:48 +0900

    Updated print function's arguments in borg.py.

--- borg.py
+++ borg.py
@@ -18,19 +18,19 @@ if __name__ == '__main__':
     rm1.state = 'Idle'
     rm2.state = 'Running'
 
-    print('rm1:', rm1)
-    print('rm2:', rm2)
+    print('rm1: {0}'.format(rm1))
+    print('rm2: {0}'.format(rm2))
 
     rm2.state = 'Zombie'
 
-    print('rm1:', rm1)
-    print('rm2:', rm2)
+    print('rm1: {0}'.format(rm1))
+    print('rm2: {0}'.format(rm2))
 
-    print('rm1 id:', id(rm1))
-    print('rm2 id:', id(rm2))
+    print('rm1 id: {0}'.format(id(rm1)))
+    print('rm2 id: {0}'.format(id(rm2)))
 
     rm3 = YourBorg()
 
-    print('rm1:', rm1)
-    print('rm2:', rm2)
-    print('rm3:', rm3)
+    print('rm1: {0}'.format(rm1))
+    print('rm2: {0}'.format(rm2))
+    print('rm3: {0}'.format(rm3))
