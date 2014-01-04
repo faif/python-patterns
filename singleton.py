b8b649c1c46b9f534f40db8f8a9f02fb112017f5
Abhishek Gahlot <abhishekgahlot007@gmail.com>
2014-01-04 20:22:23 +0530

    Create singleton.py

---
+++ singleton.py
@@ -0,0 +1,8 @@
+
+class Singleton(object):
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(
+                                cls, *args, **kwargs)
+        return cls._instance
