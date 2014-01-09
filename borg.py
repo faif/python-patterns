ca8cb4c5284b6d4e207404c579f87970768f9ab3
Jeroen <osscca.git@gmail.com>
2014-01-09 11:26:43 +0100

    Small PEP8 corrections

--- borg.py
+++ borg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 class Borg:
     __shared_state = {}
 
--- graph_search.py
+++ graph_search.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 class GraphSearch:
     """Graph search emulation in python, from source
     http://www.python.org/doc/essays/graphs/"""
