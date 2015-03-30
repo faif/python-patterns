c773edaab5f453e6cea2bc64607ff844443287bf
lord63 <lord63.j@gmail.com>
2015-03-30 22:38:47 +0800

    Pep8 check for adapter.py

--- adapter.py
+++ adapter.py
@@ -7,18 +7,23 @@
 class Dog(object):
     def __init__(self):
         self.name = "Dog"
+
     def bark(self):
         return "woof!"
+
 
 class Cat(object):
     def __init__(self):
         self.name = "Cat"
+
     def meow(self):
         return "meow!"
+
 
 class Human(object):
     def __init__(self):
         self.name = "Human"
+
     def speak(self):
         return "'hello'"
 
@@ -26,6 +31,7 @@ class Human(object):
 class Car(object):
     def __init__(self):
         self.name = "Car"
+
     def make_noise(self, octane_level):
         return "vroom{0}".format("!" * octane_level)
 
