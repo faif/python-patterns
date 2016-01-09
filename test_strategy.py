ee76a07ee9f6d7674e360c7bbb56e2b96e4fcbf2
thinwybk <thinwybk@mailbox.org>
2016-01-09 15:58:04 +0100

    add: docstring for strategy test file

--- test_strategy.py
+++ test_strategy.py
@@ -1,9 +1,18 @@
+"""
+Tests for strategy.py
+"""
+
 import unittest
 import subprocess
 
 class StrategyTest(unittest.TestCase):
 
     def test_print_output(self):
+        """
+        Verifies the print output when strategy.py is executed.
+        The expected_output is equivalent to the output on the command
+        line when running 'python strategy.py'.
+        """
         output = subprocess.check_output(["python", "strategy.py"])
         expected_output = 'Strategy Example 0\r\n\
 Strategy Example 1 from execute 1\r\n\
