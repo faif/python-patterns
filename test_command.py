7f2e34b5278f6906256ad4d4d4b875532e60f5fd
thinwybk <thinwybk@mailbox.org>
2016-01-21 23:03:44 +0100

    add: test undo / change: cls name, setup, teardown

--- test_command.py
+++ test_command.py
@@ -2,43 +2,52 @@ from command import MoveFileCommand
 import unittest, os, shutil, subprocess
 
 
-class StrategyTest(unittest.TestCase):
+class CommandTest(unittest.TestCase):
 
-    def __get_directories(self):
+    @classmethod
+    def __get_test_directory(self):
         """
-        Get the directories relevant for tests:
-        - self.file_dir: the directory of this script
-        - self.test_dir: the root directory for tests
+        Get the temporary directory for the tests.
         """
-        self.file_dir = os.path.dirname(os.path.realpath(__file__))
-        self.test_dir = os.path.join(self.file_dir, 'test_command')
+        self.test_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_command')
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         """
-        Creates a temporary directory and file:
-        ./test_command
+        - Create a temporary directory and file
+        /test_command
            /foo.txt
+        - get the temporary test directory
+        - and initializes the command stack.
         """
         os.mkdir('test_command')
         open('test_command/foo.txt', 'w').close()
-        self.__get_directories()
-
-    def test_sequential_execution(self):
+        self.__get_test_directory()
         self.command_stack = []
         self.command_stack.append(MoveFileCommand(os.path.join(self.test_dir, 'foo.txt'), os.path.join(self.test_dir, 'bar.txt')))
         self.command_stack.append(MoveFileCommand(os.path.join(self.test_dir, 'bar.txt'), os.path.join(self.test_dir, 'baz.txt')))
-        self.command_stack[0].execute()
-        output_after_first_command = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_command[0], 'bar.txt')
-        self.command_stack[1].execute()
-        output_after_second_command = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_command[0], 'baz.txt')
 
-    def tearDown(self):
+    def test_sequential_execution(self):
+        self.command_stack[0].execute()
+        output_after_first_execution = os.listdir(self.test_dir)
+        self.assertEqual(output_after_first_execution[0], 'bar.txt')
+        self.command_stack[1].execute()
+        output_after_second_execution = os.listdir(self.test_dir)
+        self.assertEqual(output_after_second_execution[0], 'baz.txt')
+
+    def test_sequential_undo(self):
+        self.command_stack = list(reversed(self.command_stack))
+        self.command_stack[0].undo()
+        output_after_first_undo = os.listdir(self.test_dir)
+        self.assertEqual(output_after_first_undo[0], 'bar.txt')
+        self.command_stack[1].undo()
+        output_after_second_undo = os.listdir(self.test_dir)
+        self.assertEqual(output_after_second_undo[0], 'foo.txt')
+
+    @classmethod
+    def tearDownClass(self):
         """
-        Removes the temporary directory and its content:
-        ./test_command
-           ...
+        Remove the temporary directory /test_command and its content.
         """
         shutil.rmtree('test_command')
 
