e6cdda75eca979537aaa8af9980e4896e9405e1e
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2016-02-29 10:38:44 +0100

    changed directory test

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -14,10 +14,12 @@ class StrategyTest(unittest.TestCase):
         The expected_output is equivalent to the output on the command
         line when running 'python strategy.py'.
         """
-        output = subprocess.check_output(["python", "strategy.py"])
+        output = subprocess.check_output(["python", "patterns/strategy.py"])
         expected_output = 'Strategy Example 0\r\n\
 Strategy Example 1 from execute 1\r\n\
 Strategy Example 2 from execute 2\r\n'
         # byte representation required due to EOF returned subprocess
         expected_output_as_bytes = expected_output.encode(encoding='UTF-8')
-        self.assertEqual(output, expected_output_as_bytes)
+        self.assertEqual(
+            output.splitlines(), expected_output_as_bytes.splitlines()
+        )
