f22c88fac74fa0825cd344c7d08b0ff7aeb70c97
fkromer <thinwybk@mailbox.org>
2016-03-01 21:25:37 +0100

    add: test for cat and dog class

--- test_abstract_factory.py
+++ test_abstract_factory.py
@@ -11,6 +11,7 @@ else:
 
 from unittest.mock import patch
 
+
 class TestPetShop(unittest.TestCase):
     
     def test_dog_pet_shop_shall_show_dog_instance(self):
@@ -30,6 +31,33 @@ class TestPetShop(unittest.TestCase):
             ps.show_pet()
             self.assertEqual(mock_f_get_pet.call_count, 1)
             self.assertEqual(mock_f_get_food.call_count, 1)
+
+
+class TestCat(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.c = Cat()
+
+    def test_cat_shall_meow(cls):
+        cls.assertEqual(cls.c.speak(), 'meow')
+
+    def test_cat_shall_be_printable(cls):
+        cls.assertEqual(str(cls.c), 'Cat')
+
+
+class TestDog(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.d = Dog()
+
+    def test_dog_shall_woof(cls):
+        cls.assertEqual(cls.d.speak(), 'woof')
+
+    def test_dog_shall_be_printable(cls):
+        cls.assertEqual(str(cls.d), 'Dog')
+
 
 if __name__ == "__main__":
     unittest.main()
