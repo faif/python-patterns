4e70c4192937bfc084dcc63bc8738c5fbad481f0
fkromer <thinwybk@mailbox.org>
2016-04-25 20:25:13 +0200

    change: refactoring self->cls

--- test_proxy.py
+++ test_proxy.py
@@ -11,93 +11,95 @@ if sys.version_info < (2, 7):
 else:
     import unittest
 
+
 class ProxyTest(unittest.TestCase):
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
         """ Class scope setup. """
-        self.p = Proxy()
+        cls.p = Proxy()
 
-    def setUp(self):
+    def setUp(cls):
         """ Function/test case scope setup. """
-        self.output = StringIO()
-        self.saved_stdout = sys.stdout
-        sys.stdout = self.output
+        cls.output = StringIO()
+        cls.saved_stdout = sys.stdout
+        sys.stdout = cls.output
 
-    def tearDown(self):
+    def tearDown(cls):
         """ Function/test case scope teardown. """
-        self.output.close()
-        sys.stdout = self.saved_stdout
+        cls.output.close()
+        sys.stdout = cls.saved_stdout
 
-    def test_sales_manager_shall_work_through_proxy_with_delay(self):
-        self.p.busy = 'No'
+    def test_sales_manager_shall_work_through_proxy_with_delay(cls):
+        cls.p.busy = 'No'
         start_time = time()
-        self.p.work()
+        cls.p.work()
         end_time = time()
         execution_time = end_time - start_time
-        print_output = self.output.getvalue()
+        print_output = cls.output.getvalue()
         expected_print_output = 'Proxy checking for Sales Manager availability\n\
 Sales Manager ready to talk\n'
-        self.assertEqual(print_output, expected_print_output)
+        cls.assertEqual(print_output, expected_print_output)
         expected_execution_time = 2
-        self.assertEqual(int(execution_time), expected_execution_time)
+        cls.assertEqual(int(execution_time), expected_execution_time)
 
-    def test_sales_manager_shall_respond_through_proxy_with_delay(self):
-        self.p.busy = 'Yes'
+    def test_sales_manager_shall_respond_through_proxy_with_delay(cls):
+        cls.p.busy = 'Yes'
         start_time = time()
-        self.p.work()
+        cls.p.work()
         end_time = time()
         execution_time = end_time - start_time
-        print_output = self.output.getvalue()
+        print_output = cls.output.getvalue()
         expected_print_output = 'Proxy checking for Sales Manager availability\n\
 Sales Manager is busy\n'
-        self.assertEqual(print_output, expected_print_output)
+        cls.assertEqual(print_output, expected_print_output)
         expected_execution_time = 2
-        self.assertEqual(int(execution_time), expected_execution_time)
+        cls.assertEqual(int(execution_time), expected_execution_time)
+
 
 class NoTalkProxyTest(unittest.TestCase):
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
         """ Class scope setup. """
-        self.ntp = NoTalkProxy()
+        cls.ntp = NoTalkProxy()
 
-    def setUp(self):
+    def setUp(cls):
         """ Function/test case scope setup. """
-        self.output = StringIO()
-        self.saved_stdout = sys.stdout
-        sys.stdout = self.output
+        cls.output = StringIO()
+        cls.saved_stdout = sys.stdout
+        sys.stdout = cls.output
 
-    def tearDown(self):
+    def tearDown(cls):
         """ Function/test case scope teardown. """
-        self.output.close()
-        sys.stdout = self.saved_stdout
+        cls.output.close()
+        sys.stdout = cls.saved_stdout
 
-    def test_sales_manager_shall_not_work_through_proxy_with_delay(self):
-        self.ntp.busy = 'No'
+    def test_sales_manager_shall_not_work_through_proxy_with_delay(cls):
+        cls.ntp.busy = 'No'
         start_time = time()
-        self.ntp.work()
+        cls.ntp.work()
         end_time = time()
         execution_time = end_time - start_time
-        print_output = self.output.getvalue()
+        print_output = cls.output.getvalue()
         expected_print_output = 'Proxy checking for Sales Manager availability\n\
 This Sales Manager will not talk to you whether he/she is busy or not\n'
-        self.assertEqual(print_output, expected_print_output)
+        cls.assertEqual(print_output, expected_print_output)
         expected_execution_time = 2
-        self.assertEqual(int(execution_time), expected_execution_time)
+        cls.assertEqual(int(execution_time), expected_execution_time)
 
-    def test_sales_manager_shall_not_respond_through_proxy_with_delay(self):
-        self.ntp.busy = 'Yes'
+    def test_sales_manager_shall_not_respond_through_proxy_with_delay(cls):
+        cls.ntp.busy = 'Yes'
         start_time = time()
-        self.ntp.work()
+        cls.ntp.work()
         end_time = time()
         execution_time = end_time - start_time
-        print_output = self.output.getvalue()
+        print_output = cls.output.getvalue()
         expected_print_output = 'Proxy checking for Sales Manager availability\n\
 This Sales Manager will not talk to you whether he/she is busy or not\n'
-        self.assertEqual(print_output, expected_print_output)
+        cls.assertEqual(print_output, expected_print_output)
         expected_execution_time = 2
-        self.assertEqual(int(execution_time), expected_execution_time)
+        cls.assertEqual(int(execution_time), expected_execution_time)
 
 if __name__ == "__main__":
     unittest.main()
