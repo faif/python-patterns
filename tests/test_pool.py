7a20d3dfaba88f8e2b1a1621c5eca1730d67b096
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-01-31 18:43:31 +0200

    Primitive test suite for object pool pattern

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -5,38 +5,29 @@ try:
     import queue
 except ImportError:  # python 2.x compatibility
     import Queue as queue
-from creational.pool import QueueObject
+from creational.pool import ObjectPool
 
 
 class TestPool(unittest.TestCase):
 
     def setUp(self):
+        self.sample_queue = queue.Queue()
+        self.sample_queue.put('first')
+        self.sample_queue.put('second')
 
-        def test_object(queue):
-            queue_object = QueueObject(queue, True)
-            print('Inside func: {}'.format(queue_object.object))
+    def test_items_recoil(self):
+        with ObjectPool(self.sample_queue, True) as pool:
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
 
-    def test_pool_behavior(self):
-        sample_queue = queue.Queue()
-        sample_queue.put('yam')
-        self.assertTrue(sample_queue.get() == 'yam')
-        # with QueueObject(sample_queue) as obj:
-        #     print('Inside with: {}'.format(obj))
-
-#     sample_queue.put('sam')
-#     test_object(sample_queue)
-#     print('Outside func: {}'.format(sample_queue.get()))
-
-#     if not sample_queue.empty():
-#         print(sample_queue.get())
-
-
-# if __name__ == '__main__':
-#     main()
-
-### OUTPUT ###
-# Inside with: yam
-# Outside with: yam
-# Inside func: sam
-# Outside func: sam
-
+    def test_frozen_pool(self):
+        with ObjectPool(self.sample_queue) as pool:
+            self.assertEqual(pool, 'first')
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
