42ba041ef31af4f36f284147e26dca60c63d37d7
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-02-10 17:50:55 +0200

    Facade desing pattern primitive test suite

---
+++ tests/test_facade.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+from structural.facade import TestRunner
+
+
+class TestRunnerFacilities(unittest.TestCase):
+
+    def setUp(self):
+        self.runner = TestRunner()
+        self.average_result = """###### In Test 1 ######
+Setting up
+Running test
+Tearing down
+Test Finished
+
+###### In Test 2 ######
+Setting up
+Running test
+Tearing down
+Test Finished
+
+###### In Test 3 ######
+Setting up
+Running test
+Tearing down
+Test Finished"""
+
+    def test_bunch_launch(self):
+        import sys
+        from io import StringIO
+        out = StringIO()
+        sys.stdout = out
+        self.runner.runAll()
+        output = out.getvalue().strip()
+        self.assertEqual(output, self.average_result)
+
+
