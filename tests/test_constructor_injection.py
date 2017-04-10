4cca032c7e3de6a31a7715090f9b0ffd3c6dfa86
Florian Kromer <thinwybk@mailbox.org>
2017-04-10 20:45:29 +0200

    change: string concatenations

--- tests/test_constructor_injection.py
+++ tests/test_constructor_injection.py
@@ -40,5 +40,8 @@ class ConstructorInjectionTest(unittest.TestCase):
         production_code_time_provider = ProductionCodeTimeProvider()
         class_under_test = TimeDisplay(production_code_time_provider)
         current_time = datetime.datetime.now()
-        expected_time = "<span class=\"tinyBoldText\">" + str(current_time.hour) + ":" + str(current_time.minute) + "</span>"
+        expected_time = "<span class=\"tinyBoldText\">{}:{}</span>".format(current_time.hour, current_time.minute)
         self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+
+if __name__ == "__main__":
+    unittest.main()
