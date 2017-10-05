e09df34aef5c2eca63aa13cc9d92f9af14dd375c
lucasloisp <lucasloisp@gmail.com>
2017-10-04 22:28:53 -0300

    Adds description to Object Pool

--- creational/pool.py
+++ creational/pool.py
@@ -2,9 +2,31 @@
 # -*- coding: utf-8 -*-
 
 """
-http://stackoverflow.com/questions/1514120/python-implementation-of-the-object-pool-design-pattern
-"""
+*What is this pattern about?
+This pattern is used when creating an object is costly (and they are
+created frequently) but only a few are used at a time. With a Pool we
+can manage those instances we have as of now by caching them. Now it
+is possible to skip the costly creation of an object if one is
+available in the pool.
+A pool allows to 'check out' an inactive object and then to return it.
+If none are available the pool creates one to provide without wait.
 
+*What does this example do?
+In this example queue.Queue is used to create the pool (wrapped in a
+custom ObjectPool object to use with the with statement), and it is
+populated with strings.
+As we can see, the first string object put in "yam" is USED by the
+with statement. But because it is released back into the pool
+aftwerwards it is reused by the explicit call to sample_queue.get().
+Same thing happens with "sam", when the ObjectPool created insided the
+function is deleted (by the GC) and the object is returned.
+
+*Where is the pattern used practically?
+
+*References:
+http://stackoverflow.com/questions/1514120/python-implementation-of-the-object-pool-design-pattern
+https://sourcemaking.com/design_patterns/object_pool
+"""
 
 
 class ObjectPool(object):
