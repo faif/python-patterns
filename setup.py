af12e8fc20000af4ad73fa13758ea76ba66ca118
Grygorii Iermolenko <gyermolenko@gmail.com>
2019-01-13 17:02:28 +0200

    remove 3.7 from CI, fix flake warnings

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='python-patterns',
-    description='A collection of design patterns and idioms in Python.',
+    name="python-patterns",
+    description="A collection of design patterns and idioms in Python.",
     classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
 )
