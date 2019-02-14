ac7709e37c683cd3648f1d7dd5dc38ea823e6222
Grygorii Iermolenko <gyermolenko@gmail.com>
2019-02-14 11:58:37 +0200

    Doctest Template

--- patterns/behavioral/template.py
+++ patterns/behavioral/template.py
@@ -50,29 +50,25 @@ def template_function(getter, converter=False, to_save=False):
 
 
 def main():
-    template_function(get_text, to_save=True)
-    print("-" * 30)
-    template_function(get_pdf, converter=convert_to_text)
-    print("-" * 30)
-    template_function(get_csv, to_save=True)
+    """
+    >>> template_function(get_text, to_save=True)
+    Got `plain-text`
+    Skip conversion
+    [SAVE]
+    `plain-text` was processed
 
+    >>> template_function(get_pdf, converter=convert_to_text)
+    Got `pdf`
+    [CONVERT]
+    `pdf as text` was processed
+
+    >>> template_function(get_csv, to_save=True)
+    Got `csv`
+    Skip conversion
+    [SAVE]
+    `csv` was processed
+    """
 
 if __name__ == "__main__":
-    main()
-
-
-OUTPUT = """
-Got `plain-text`
-Skip conversion
-[SAVE]
-`plain-text` was processed
-------------------------------
-Got `pdf`
-[CONVERT]
-`pdf as text` was processed
-------------------------------
-Got `csv`
-Skip conversion
-[SAVE]
-`csv` was processed
-"""
+    import doctest
+    doctest.testmod()
