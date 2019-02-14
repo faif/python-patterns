b50a6e87a4c3e9b60f52452c13a14d03c569463a
Grygorii Iermolenko <gyermolenko@gmail.com>
2019-02-14 15:04:55 +0200

    Doctest Adapter

--- patterns/structural/adapter.py
+++ patterns/structural/adapter.py
@@ -70,28 +70,6 @@ class Adapter(object):
     Usage:
     dog = Dog()
     dog = Adapter(dog, make_noise=dog.bark)
-
-    >>> objects = []
-    >>> dog = Dog()
-    >>> print(dog.__dict__)
-    {'name': 'Dog'}
-    >>> objects.append(Adapter(dog, make_noise=dog.bark))
-    >>> print(objects[0].original_dict())
-    {'name': 'Dog'}
-    >>> cat = Cat()
-    >>> objects.append(Adapter(cat, make_noise=cat.meow))
-    >>> human = Human()
-    >>> objects.append(Adapter(human, make_noise=human.speak))
-    >>> car = Car()
-    >>> car_noise = lambda: car.make_noise(3)
-    >>> objects.append(Adapter(car, make_noise=car_noise))
-
-    >>> for obj in objects:
-    ...     print('A {} goes {}'.format(obj.name, obj.make_noise()))
-    A Dog goes woof!
-    A Cat goes meow!
-    A Human goes 'hello'
-    A Car goes vroom!!!
     """
 
     def __init__(self, obj, **adapted_methods):
@@ -109,32 +87,36 @@ class Adapter(object):
 
 
 def main():
+    """
+    >>> objects = []
+    >>> dog = Dog()
+    >>> print(dog.__dict__)
+    {'name': 'Dog'}
 
-    objects = []
-    dog = Dog()
-    print(dog.__dict__)
-    objects.append(Adapter(dog, make_noise=dog.bark))
-    print(objects[0].__dict__)
-    print(objects[0].original_dict())
-    cat = Cat()
-    objects.append(Adapter(cat, make_noise=cat.meow))
-    human = Human()
-    objects.append(Adapter(human, make_noise=human.speak))
-    car = Car()
-    objects.append(Adapter(car, make_noise=lambda: car.make_noise(3)))
+    >>> objects.append(Adapter(dog, make_noise=dog.bark))
 
-    for obj in objects:
-        print("A {0} goes {1}".format(obj.name, obj.make_noise()))
+    >>> objects[0].__dict__['obj'], objects[0].__dict__['make_noise']
+    (<...Dog object at 0x...>, <bound method Dog.bark of <...Dog object at 0x...>>)
+
+    >>> print(objects[0].original_dict())
+    {'name': 'Dog'}
+
+    >>> cat = Cat()
+    >>> objects.append(Adapter(cat, make_noise=cat.meow))
+    >>> human = Human()
+    >>> objects.append(Adapter(human, make_noise=human.speak))
+    >>> car = Car()
+    >>> objects.append(Adapter(car, make_noise=lambda: car.make_noise(3)))
+
+    >>> for obj in objects:
+    ...    print("A {0} goes {1}".format(obj.name, obj.make_noise()))
+    A Dog goes woof!
+    A Cat goes meow!
+    A Human goes 'hello'
+    A Car goes vroom!!!
+    """
 
 
 if __name__ == "__main__":
-    main()
-
-### OUTPUT ###
-# {'name': 'Dog'}
-# {'make_noise': <bound method Dog.bark of <__main__.Dog object at 0x7f631ba3fb00>>, 'obj': <__main__.Dog object at 0x7f631ba3fb00>}    # noqa flake8
-# {'name': 'Dog'}
-# A Dog goes woof!
-# A Cat goes meow!
-# A Human goes 'hello'
-# A Car goes vroom!!!
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
