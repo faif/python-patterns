4a6aed773b24857969b1380d7d2492d1fb5a2bd3
Grygorii Iermolenko <gyermolenko@gmail.com>
2019-05-01 13:26:06 +0300

    Doctest for observer

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -8,8 +8,6 @@ import sys
 
 import pytest
 
-from patterns.behavioral.observer import main as observer_main
-from patterns.behavioral.observer import OUTPUT as observer_output
 from patterns.behavioral.publish_subscribe import main as publish_subscribe_main
 from patterns.behavioral.publish_subscribe import OUTPUT as publish_subscribe_output
 from patterns.behavioral.specification import main as specification_main
@@ -21,7 +19,6 @@ from patterns.behavioral.state import OUTPUT as state_output
 @pytest.mark.skipif(sys.version_info < (3,4),
                     reason="requires python3.4 or higher")
 @pytest.mark.parametrize("main,output", [
-    (observer_main, observer_output),
     (publish_subscribe_main, publish_subscribe_output),
     (specification_main, specification_output),
     (state_main, state_output),
