25373561bc3b7593b557b66a9704cf8b589dbca3
Julio Cesar Eiras Melanda <jcemelanda@gmail.com>
2019-10-29 11:38:21 +0100

    Changes run_all to run all the tests with coverage properly

--- run_all.sh
+++ run_all.sh
@@ -6,17 +6,4 @@
 set -eu
 failed=""
 
-if which coverage > /dev/null; then
-    COVERAGE="`which coverage` run -a"
-else
-    COVERAGE=''
-fi
-for f in */[^_]*py; do
-    PYTHONPATH=. python $COVERAGE $f || failed+=" $f"
-    echo "I: done $f. Exit code $?"
-done;
-
-if [ ! -z "$failed" ]; then
-    echo "Failed: $failed";
-    exit 1
-fi
+pytest --doctest-modules --cov=patterns
