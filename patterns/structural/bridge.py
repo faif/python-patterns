b62a0a860bf8f64667b606435f52a4f432b17270
Grygorii Iermolenko <gyermolenko@gmail.com>
2019-11-15 11:31:21 +0200

    Add doctest for bridge

--- patterns/structural/bridge.py
+++ patterns/structural/bridge.py
@@ -37,16 +37,17 @@ class CircleShape:
 
 
 def main():
-    shapes = (CircleShape(1, 2, 3, DrawingAPI1()), CircleShape(5, 7, 11, DrawingAPI2()))
+    """
+    >>> shapes = (CircleShape(1, 2, 3, DrawingAPI1()), CircleShape(5, 7, 11, DrawingAPI2()))
 
-    for shape in shapes:
-        shape.scale(2.5)
-        shape.draw()
+    >>> for shape in shapes:
+    ...    shape.scale(2.5)
+    ...    shape.draw()
+    API1.circle at 1:2 radius 7.5
+    API2.circle at 5:7 radius 27.5
+    """
 
 
 if __name__ == '__main__':
-    main()
-
-### OUTPUT ###
-# API1.circle at 1:2 radius 7.5
-# API2.circle at 5:7 radius 27.5
+    import doctest
+    doctest.testmod()
