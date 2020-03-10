b2df6c0830fbca0d2a22a5e33bd850e272f06f7a
Hojat Modaresi Zade <modaresi.hojat@gmail.com>
2020-03-10 09:24:48 +0330

    changed line len to 80 char

--- patterns/structural/proxy.py
+++ patterns/structural/proxy.py
@@ -1,6 +1,10 @@
 """
 *What is this pattern about?
-Proxy is used in places where you want to add functionality to a class without changing its interface. The main class is called `Real Subject`. A client should use the proxy or the real subject without any code change, so both must have the same interface. Logging and controlling access to the real subject are some of the proxy pattern usages.
+Proxy is used in places where you want to add functionality to a class without
+changing its interface. The main class is called `Real Subject`. A client should
+use the proxy or the real subject without any code change, so both must have the
+same interface. Logging and controlling access to the real subject are some of
+the proxy pattern usages.
 
 *References:
 https://refactoring.guru/design-patterns/proxy/python/example
