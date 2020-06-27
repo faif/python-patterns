44ce0a51513952a51c44a10493bc059342ee9192
rednafi <redowan.nafi@gmail.com>
2020-06-28 01:01:24 +0600

    Type checked adapter pattern

--- patterns/structural/adapter.py
+++ patterns/structural/adapter.py
@@ -28,58 +28,63 @@ http://python-3-patterns-idioms-test.readthedocs.io/en/latest/ChangeInterface.ht
 Allows the interface of an existing class to be used as another interface.
 """
 
+from typing import Callable, TypeVar
+
+T = TypeVar("T")
+
 
 class Dog:
-    def __init__(self):
+    def __init__(self) -> None:
         self.name = "Dog"
 
-    def bark(self):
+    def bark(self) -> str:
         return "woof!"
 
 
 class Cat:
-    def __init__(self):
+    def __init__(self) -> None:
         self.name = "Cat"
 
-    def meow(self):
+    def meow(self) -> str:
         return "meow!"
 
 
 class Human:
-    def __init__(self):
+    def __init__(self) -> None:
         self.name = "Human"
 
-    def speak(self):
+    def speak(self) -> str:
         return "'hello'"
 
 
 class Car:
-    def __init__(self):
+    def __init__(self) -> None:
         self.name = "Car"
 
-    def make_noise(self, octane_level):
-        return "vroom{0}".format("!" * octane_level)
+    def make_noise(self, octane_level: int) -> str:
+        return f"vroom{'!' * octane_level}"
 
 
 class Adapter:
-    """
-    Adapts an object by replacing methods.
-    Usage:
+    """Adapts an object by replacing methods.
+
+    Usage
+    ------
     dog = Dog()
     dog = Adapter(dog, make_noise=dog.bark)
     """
 
-    def __init__(self, obj, **adapted_methods):
-        """We set the adapted methods in the object's dict"""
+    def __init__(self, obj: T, **adapted_methods: Callable):
+        """We set the adapted methods in the object's dict."""
         self.obj = obj
         self.__dict__.update(adapted_methods)
 
     def __getattr__(self, attr):
-        """All non-adapted calls are passed to the object"""
+        """All non-adapted calls are passed to the object."""
         return getattr(self.obj, attr)
 
     def original_dict(self):
-        """Print original object dict"""
+        """Print original object dict."""
         return self.obj.__dict__
 
 
@@ -116,4 +121,5 @@ def main():
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod(optionflags=doctest.ELLIPSIS)
