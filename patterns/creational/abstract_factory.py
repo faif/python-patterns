55f3bf59ff8fb95f21224f1f374abb91379d7444
tony7285 <72184919+tony7285@users.noreply.github.com>
2020-10-02 23:16:39 +0530

    Improve spellings

--- patterns/creational/abstract_factory.py
+++ patterns/creational/abstract_factory.py
@@ -1,18 +1,18 @@
-"""
-*What is this pattern about?
 
-In Java and other languages, the Abstract Factory Pattern serves to provide an interface for
+1 What is this pattern about?
+
+* In Java and other languages, the Abstract Factory Pattern serves to provide an interface for
 creating related/dependent objects without need to specify their
 actual class.
 
-The idea is to abstract the creation of objects depending on business
+* The idea is to abstract the creation of objects depending on business
 logic, platform choice, etc.
 
-In Python, the interface we use is simply a callable, which is "builtin" interface
+* In Python, the interface we use is simply a callable, which is "builtin" interface
 in Python, and in normal circumstances we can simply use the class itself as
 that callable, because classes are first class objects in Python.
 
-*What does this example do?
+2 What does this example do?
 This particular implementation abstracts the creation of a pet and
 does so depending on the factory we chose (Dog or Cat, or random_animal)
 This works because both Dog/Cat and random_animal respect a common
@@ -20,7 +20,7 @@ interface (callable for creation and .speak()).
 Now my application can create pets abstractly and decide later,
 based on my own criteria, dogs over cats.
 
-*Where is the pattern used practically?
+3 Where is the pattern used practically?
 
 *References:
 https://sourcemaking.com/design_patterns/abstract_factory
