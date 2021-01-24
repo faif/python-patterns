74eb6343f846ed3a220a2c078dd4901cd0be0957
Yusuke Hayashi <yusuke8h@gmail.com>
2021-01-24 16:31:06 +0900

    Update test of abstract factory

--- tests/creational/test_abstract_factory.py
+++ tests/creational/test_abstract_factory.py
@@ -8,5 +8,6 @@ class TestPetShop(unittest.TestCase):
     def test_dog_pet_shop_shall_show_dog_instance(self):
         dog_pet_shop = PetShop(Dog)
         with patch.object(Dog, "speak") as mock_Dog_speak:
-            dog_pet_shop.show_pet()
+            pet = dog_pet_shop.buy_pet("")
+            pet.speak()
             self.assertEqual(mock_Dog_speak.call_count, 1)
