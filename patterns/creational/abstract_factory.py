f72b1ba1b466c5dd1f772e69cd90e18ff6f834be
Yusuke Hayashi <yusuke8h@gmail.com>
2021-01-24 16:17:17 +0900

    Update abstract_factory more elaborate

--- patterns/creational/abstract_factory.py
+++ patterns/creational/abstract_factory.py
@@ -31,71 +31,85 @@ Provides a way to encapsulate a group of individual factories.
 """
 
 import random
+from typing import Type
+
+
+class Pet:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def speak(self) -> None:
+        raise NotImplementedError
+
+    def __str__(self) -> str:
+        raise NotImplementedError
+
+
+class Dog(Pet):
+    def speak(self) -> None:
+        print("woof")
+
+    def __str__(self) -> str:
+        return f"Dog<{self.name}>"
+
+
+class Cat(Pet):
+    def speak(self) -> None:
+        print("meow")
+
+    def __str__(self) -> str:
+        return f"Cat<{self.name}>"
 
 
 class PetShop:
 
     """A pet shop"""
 
-    def __init__(self, animal_factory=None):
+    def __init__(self, animal_factory: Type[Pet]) -> None:
         """pet_factory is our abstract factory.  We can set it at will."""
 
         self.pet_factory = animal_factory
 
-    def show_pet(self):
+    def buy_pet(self, name: str) -> Pet:
         """Creates and shows a pet using the abstract factory"""
 
-        pet = self.pet_factory()
-        print(f"We have a lovely {pet}")
-        print(f"It says {pet.speak()}")
-
-
-class Dog:
-    def speak(self):
-        return "woof"
-
-    def __str__(self):
-        return "Dog"
-
-
-class Cat:
-    def speak(self):
-        return "meow"
-
-    def __str__(self):
-        return "Cat"
+        pet = self.pet_factory(name)
+        print(f"Here is your lovely {pet}")
+        return pet
 
 
 # Additional factories:
 
 # Create a random animal
-def random_animal():
+def random_animal(name: str) -> Pet:
     """Let's be dynamic!"""
-    return random.choice([Dog, Cat])()
+    return random.choice([Dog, Cat])(name)
 
 
 # Show pets with various factories
-def main():
+def main() -> None:
     """
     # A Shop that sells only cats
     >>> cat_shop = PetShop(Cat)
-    >>> cat_shop.show_pet()
-    We have a lovely Cat
-    It says meow
+    >>> pet = cat_shop.buy_pet("Lucy")
+    Here is your lovely Cat<Lucy>
+    >>> pet.speak()
+    meow
 
     # A shop that sells random animals
     >>> shop = PetShop(random_animal)
-    >>> for i in range(3):
-    ...    shop.show_pet()
+    >>> for name in ["Max", "Jack", "Buddy"]:
+    ...    pet = shop.buy_pet(name)
+    ...    pet.speak()
     ...    print("=" * 20)
-    We have a lovely Cat
-    It says meow
+    Here is your lovely Cat<Max>
+    meow
     ====================
-    We have a lovely Dog
-    It says woof
+    Here is your lovely Dog<Jack>
+    woof
     ====================
-    We have a lovely Dog
-    It says woof
+    Here is your lovely Dog<Buddy>
+    woof
     ====================
     """
 
