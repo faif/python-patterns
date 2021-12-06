d752949b0efacaff1f300096f0dc4f71ae70a428
Yeonggwang <immutable000@gmail.com>
2021-12-06 10:35:04 +0900

    Added type hints to iterator pattern

--- patterns/behavioral/iterator_alt.py
+++ patterns/behavioral/iterator_alt.py
@@ -4,6 +4,7 @@ Implementation of the iterator pattern using the iterator protocol from Python
 *TL;DR
 Traverses a container and accesses the container's elements.
 """
+from __future__ import annotations
 
 
 class NumberWords:
@@ -17,14 +18,14 @@ class NumberWords:
         "five",
     )
 
-    def __init__(self, start, stop):
+    def __init__(self, start: int, stop: int) -> None:
         self.start = start
         self.stop = stop
 
-    def __iter__(self):  # this makes the class an Iterable
+    def __iter__(self) -> NumberWords:  # this makes the class an Iterable
         return self
 
-    def __next__(self):  # this makes the class an Iterator
+    def __next__(self) -> str:  # this makes the class an Iterator
         if self.start > self.stop or self.start > len(self._WORD_MAP):
             raise StopIteration
         current = self.start
