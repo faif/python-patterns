0ef9e80c2b4dfa427b163cd2900ca95c689c8583
Yeonggwang <immutable000@gmail.com>
2021-12-06 11:45:19 +0900

    Change string literals to class name. [PEP-0563]

--- patterns/creational/prototype.py
+++ patterns/creational/prototype.py
@@ -20,6 +20,7 @@ copies of the prototype: 'default', 'objecta' and 'objectb'.
 *TL;DR
 Creates new object instances by cloning prototype.
 """
+from __future__ import annotations
 
 from typing import Any, Dict
 
@@ -29,7 +30,7 @@ class Prototype:
         self.value = value
         self.__dict__.update(attrs)
 
-    def clone(self, **attrs: Any) -> "Prototype":
+    def clone(self, **attrs: Any) -> Prototype:
         """Clone a prototype and update inner attributes dictionary"""
         # Python in Practice, Mark Summerfield
         # copy.deepcopy can be used instead of next line.
