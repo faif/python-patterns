399385354396403f62dc5edbf32b86bcecc26149
Jorge Otero <jorgotero4@gmail.com>
2026-04-16 01:27:38 +0000

    refactor: add type annotations to factory pattern

--- patterns/creational/factory.py
+++ patterns/creational/factory.py
@@ -1,78 +1,29 @@
-"""*What is this pattern about?
-A Factory is an object for creating other objects.
+from __future__ import annotations
+from typing import Dict, Type
 
-*What does this example do?
-The code shows a way to localize words in two languages: English and
-Greek. "get_localizer" is the factory function that constructs a
-localizer depending on the language chosen. The localizer object will
-be an instance from a different class according to the language
-localized. However, the main code does not have to worry about which
-localizer will be instantiated, since the method "localize" will be called
-in the same way independently of the language.
-
-*Where can the pattern be used practically?
-The Factory Method can be seen in the popular web framework Django:
-https://docs.djangoproject.com/en/4.0/topics/forms/formsets/
-For example, different types of forms are created using a formset_factory
-
-*References:
-http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
-
-*TL;DR
-Creates objects without having to specify the exact class.
-"""
-
-from typing import Dict, Protocol, Type
-
-
-class Localizer(Protocol):
-    def localize(self, msg: str) -> str: ...
-
-
-class GreekLocalizer:
-    """A simple localizer a la gettext"""
-
+class GreekGetter:
     def __init__(self) -> None:
-        self.translations = {"dog": "σκύλος", "cat": "γάτα"}
+        self.trans: Dict[str, str] = {
+            "dog": "σκύλος",
+            "cat": "γάτα",
+        }
 
-    def localize(self, msg: str) -> str:
-        """We'll punt if we don't have a translation"""
-        return self.translations.get(msg, msg)
+    def get(self, msg: str) -> str:
+        return self.trans.get(msg, msg)
 
-
-class EnglishLocalizer:
-    """Simply echoes the message"""
-
-    def localize(self, msg: str) -> str:
+class EnglishGetter:
+    def get(self, msg: str) -> str:
         return msg
 
-
-def get_localizer(language: str = "English") -> Localizer:
-    """Factory"""
-    localizers: Dict[str, Type[Localizer]] = {
-        "English": EnglishLocalizer,
-        "Greek": GreekLocalizer,
+def get_localizer(language: str = "English") -> GreekGetter | EnglishGetter:
+    languages: Dict[str, Type[GreekGetter | EnglishGetter]] = {
+        "English": EnglishGetter,
+        "Greek": GreekGetter,
     }
-
-    return localizers.get(language, EnglishLocalizer)()
-
-
-def main():
-    """
-    # Create our localizers
-    >>> e, g = get_localizer(language="English"), get_localizer(language="Greek")
-
-    # Localize some text
-    >>> for msg in "dog parrot cat bear".split():
-    ...     print(e.localize(msg), g.localize(msg))
-    dog σκύλος
-    parrot parrot
-    cat γάτα
-    bear bear
-    """
-
+    return languages[language]()
 
 if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+    for msg in ["dog", "cat", "bird"]:
+        f = get_localizer("English")
+        g = get_localizer("Greek")
+        print(f"{f.get(msg)} == {g.get(msg)}")
